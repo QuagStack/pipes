d6bbb62c3b65e0a6629f7dae551d4ccd8323a692
QuagStack <62340926+QuagStack@users.noreply.github.com>
2020-05-14 13:28:42 -0400

    Initial Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  date: string
+  name: string;
+  amount: number;
+  height: number;
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+  miles: number;
+
+  onNameChange(value: string){
+    this.name = value;
+  }
+  onDateChange(value: string){
+    this.date = value;
+  }
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
+  onMilesChange(value: string){
+    this.miles = parseFloat(value);
+  }
 }
